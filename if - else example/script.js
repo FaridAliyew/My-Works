463f8feb89ce1d85997de499077a27b02aa5a14a
FaridAliyev <feliyev636@gmail.com>
2024-07-06 09:35:10 +0400

    Add new example

--- if - else example/script.js
+++ if - else example/script.js
@@ -1,13 +1,13 @@
 // ------------ My-Task-1  --->  (İstifadəçi yaşını yoxlayın) 
-while (true) {
-    const age = Number(prompt("Yaşınızı daxil edin:"));
+// while (true) {
+//     const age = Number(prompt("Yaşınızı daxil edin:"));
 
-    if (age > 18) {
-        alert("Yetkinlik yaşınıza çatmısınız..");
-    } else {
-        alert("Yetkinlik yaşınıza çatmamısınız!!");
-    }
-}
+//     if (age > 18) {
+//         alert("Yetkinlik yaşınıza çatmısınız..");
+//     } else {
+//         alert("Yetkinlik yaşınıza çatmamısınız!!");
+//     }
+// }
 
 
 
@@ -82,4 +82,99 @@ while (true) {
 //     } else {
 //         alert("İmtahandan kəsildiniz.." + "\n" + "Balınız:" + " " + averageHoney);
 //     }
+// }
+
+
+
+
+// // ---------------- My-Task-6  ---> (Kilonun təyin olunması)
+// const weight = Number(prompt("Çəkinizi daxil edin (Kq)"));
+// const height = Number(prompt("Bouyunuzu daxil edin (metr olaraq)"));
+// const result = weight / height ** 2;
+
+// const check = () => {
+//     if (result <= 18.5) {
+//         alert("Arıqsınız" + " " + result);
+//         return;
+//     }
+
+//     if (result >= 18.9 && result <= 24.9) {
+//         alert("Normal kilodasınız" + " " + result);
+//         return;
+//     }
+
+//     if (result >= 25 && result <= 29.9) {
+//         alert("Artıq çəkili" + " " + result);
+//         return;
+//     }
+
+//     if (result >= 30) {
+//         alert("Piylənmə" + " " + result);
+//         return;
+//     }
+// }
+// check();
+
+
+
+
+// ---------------- My-Task-7  ---> (Benzinin Alınması)
+const premium = 1.50;
+const a92 = 1;
+const dizel = 0.80;
+const newLine = "\r\n";
+
+const benzin = prompt("Benzin növünü seçin:" + newLine
+    + "1-Premium" + newLine
+    + "2-A-92" + newLine
+    + "3-Dizel");
+
+if (benzin == 1 || benzin == 2 || benzin == 3) {
+    const litr = Number(prompt("Litri daxil edin"));
+    const balance = Number(prompt("Balansınızı daxil edin"));
+
+    if (benzin == 1) {
+        let result = premium * litr;
+        if (result <= balance) {
+            alert("Ödəmə tamamlanmışdır, Balansınız: " + (balance - result) + " AZN");
+        } else {
+            alert("Balansınızda kifayət qədər pul yoxdur");
+        }
+
+    } else if (benzin == 2) {
+        let result = a92 * litr;
+        if (result <= balance) {
+            alert("Ödəmə tamamlanmışdır, Balansınız: " + (balance - result) + " AZN");
+        } else {
+            alert("Balansınızda kifayət qədər pul yoxdur");
+        }
+
+    } else if (benzin == 3) {
+        let result = dizel * litr;
+        if (result <= balance) {
+            alert("Ödəmə tamamlanmışdır, Balansınız: " + (balance - result) + " AZN");
+        } else {
+            alert("Balansınızda kifayət qədər pul yoxdur");
+        }
+    }
+} else {
+    alert("Zəhmət olmasa 1-3 arası əməliyyat seçin");
+}
+
+
+
+
+//  ---------------- My-Task-8  ---> (Ən böyük ədədin tapılması)
+// const numberOne = Number(prompt("Bir reqem daxil edin"));
+// const numberTwo = Number(prompt("Ikinci reqemi daxil edin"));
+// const numberThree = Number(prompt("Ucuncu reqemi daxil edin"))
+
+// if (numberOne === numberTwo && numberOne === numberThree) {
+//     alert("Daxil etdiyiniz üç rəqəm də bir-birinə bərabərdir");
+// } else if (numberOne >= numberTwo && numberOne >= numberThree) {
+//     alert("Ən böyük ədəd: " + numberOne);
+// } else if (numberTwo >= numberOne && numberTwo >= numberThree) {
+//     alert("Ən böyük ədəd: " + numberTwo);
+// } else {
+//     alert("Ən böyük ədəd: " + numberThree);
 // }
